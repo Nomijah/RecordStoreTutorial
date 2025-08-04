29f03c1230d6a6aabe4fc5127ad133e6dae42ce3
Nomijah <petter.bostrom@gmail.com>
2025-08-04 16:19:37 +0200

    Fixed forgotten commented out code

--- RecordStoreTutorial/Program.cs
+++ RecordStoreTutorial/Program.cs
@@ -44,15 +44,15 @@ namespace RecordStore.WebAPI
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
+            //Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Record Store API V1");
                 });
-            //}
+            }
 
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
