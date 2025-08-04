c85a7a52edbadcb68acced1bc75faa8bb969119c
Nomijah <petter.bostrom@gmail.com>
2025-08-04 12:58:31 +0200

    interfaces added to core lib

---
+++ RecordStoreTutorial.Core/Interfaces/IArtistRepository.cs
@@ -0,0 +1,17 @@
+﻿using RecordStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordStore.Core.Interfaces
+{
+    public interface IArtistRepository : IRepository<Artist>
+    {
+        Task<IEnumerable<Artist>> GetArtistsWithAlbumsAsync();
+        Task<Artist?> GetArtistWithAlbumsAsync(int id);
+        Task<IEnumerable<Artist>> GetActiveArtistsAsync();
+        Task<IEnumerable<Artist>> GetArtistsByCountryAsync(string country);
+    }
+}
---
+++ RecordStoreTutorial.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using RecordStore.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordStore.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAlbumRepository Albums { get; }
+        IArtistRepository Artists { get; }
+        IRepository<Genre> Genres { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
