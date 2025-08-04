3c8fbe22eb4b9090acadd9ecf202486d76abc655
Nomijah <petter.bostrom@gmail.com>
2025-08-04 14:12:47 +0200

    Added infrastructure layer and classes

---
+++ RecordStore.Infrastructure/Repositories/ArtistRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordStore.Core.Interfaces;
+using RecordStore.Core.Models;
+using RecordStore.Infrastructure.Data;
+
+namespace RecordStore.Infrastructure.Repositories
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(RecordStoreDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Artist>> GetArtistsWithAlbumsAsync()
+        {
+            return await _dbSet.Include(a => a.Albums).ToListAsync();
+        }
+
+        public async Task<Artist?> GetArtistWithAlbumsAsync(int id)
+        {
+            return await _dbSet.Include(a => a.Albums)
+                              .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task<IEnumerable<Artist>> GetActiveArtistsAsync()
+        {
+            return await _dbSet.Where(a => a.IsActive).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Artist>> GetArtistsByCountryAsync(string country)
+        {
+            return await _dbSet.Where(a => a.Country == country).ToListAsync();
+        }
+    }
+}
---
+++ RecordStore.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordStore.Core.Interfaces;
+using RecordStore.Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace RecordStore.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly RecordStoreDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(RecordStoreDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public virtual async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            return entity;
+        }
+
+        public virtual async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public virtual async Task<bool> ExistsAsync(int id)
+        {
+            return await _dbSet.FindAsync(id) != null;
+        }
+    }
+}
